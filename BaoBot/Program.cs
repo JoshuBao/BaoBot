71d1d041f74c8482c8a027dac3bc7acbf2fdc9e0
Kosi Nwabueze <kosinwabueze@gmail.com>
2018-09-01 18:44:20 -0700

    Modified project to .NET Core Console

---
+++ BaoBot/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BaoBot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => new Program().TaskMain().GetAwaiter().GetResult();
+
+        public async Task TaskMain()
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
